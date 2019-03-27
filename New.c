9116a69158abb20a3d145a28dbcfed45b9c0d3db
Rishi3096 <48976736+Rishi3096@users.noreply.github.com>
2019-03-27 12:21:18 +0530

    Update New.c

--- New.c
+++ New.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 int main()
 {
-    double number;
+    int n, i, flag = 0;
 
-    number= 23.0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
 
-    // true if number is less than 0
-    if (number < 0.0)
-        printf("You entered a negative number.");
+    for(i = 2; i <= n/2; ++i)
+    {
+        // condition for nonprime number
+        if(n%i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
 
-    // true if number is greater than 0
-    else if ( number > 0.0)
-        printf("You entered a positive number.");
-
-    // if both test expression is evaluated to false
-    else
-        printf("You entered 0.");
+    if (n == 1) 
+    {
+      printf("1 is neither a prime nor a composite number.");
+    }
+    else 
+    {
+        if (flag == 0)
+          printf("%d is a prime number.", n);
+        else
+          printf("%d is not a prime number.", n);
+    }
+    
     return 0;
 }
